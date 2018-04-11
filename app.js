586ca599ac91ab2df6c07ed2fe5b6f36c2b20568
Florian MICHAUT <fmichaut@adneom.com>
2018-04-11 15:11:03 +0200

    chore(): change cors to match firebase

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes.js");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); //<-- you can change this with a specific url like http://localhost:4200
+app.use(function(req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://dough-4f780.firebaseapp.com"
+  ); //<-- you can change this with a specific url like http://localhost:4200
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
@@ -21,6 +24,6 @@ app.use(function (req, res, next) {
 
 app.use("/api", routes);
 
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port %s...", server.address().port);
 });
